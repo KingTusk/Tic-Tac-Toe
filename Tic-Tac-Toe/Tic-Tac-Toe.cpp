307011e9e82a2507249b53fb50b7b3dbd87394eb
KingTusk <70224576+KingTusk@users.noreply.github.com>
2020-09-15 10:52:00 +0200

    new stuff added

--- Tic-Tac-Toe/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -1,11 +1,10 @@
-// Tic-Tac-Toe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
     char table[9] = { '1','2','3','4','5','6','7','8','9' };
+
+
     for (int i = 0; i < sizeof(table)/sizeof(table[0]); i++) {
         if (i % 3 == 0) {
             std::cout << "\n\n";
